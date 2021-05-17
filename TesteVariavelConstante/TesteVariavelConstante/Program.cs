94fe79bceed90c66bd57ac2f4bdbadabec93ac63
Lucas Menezes <lucashenrique281@hotmail.com>
2021-05-17 13:07:34 -0300

    Create Program.cs

---
+++ TesteVariavelConstante/TesteVariavelConstante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TesteVariavelConstante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string constante = "Uma constante que não muda";
+
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Por favor informar um número inteiro");
+                return;
+            }
+            
+            int inteiro;
+            bool test = int.TryParse(args[0], out inteiro);
+
+            if(test == false)
+            {
+                Console.WriteLine("O argumento informado não é um número inteiro");
+                return;
+            }
+
+            Console.WriteLine(constante);
+            Console.WriteLine("O número informado foi: " + inteiro);
+        }
+    }
+}
