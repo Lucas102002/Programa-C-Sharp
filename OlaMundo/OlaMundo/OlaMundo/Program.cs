31e2a4cb783e28586478df1e7ccf0d385b2763c6
Lucas Menezes <lucashenrique281@hotmail.com>
2021-05-17 13:06:09 -0300

    Create Program.cs

---
+++ OlaMundo/OlaMundo/OlaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo,meu primeiro código C# de 2021");
+        }
+    }
+}
